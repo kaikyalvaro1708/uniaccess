7c5454d05db72d0f3073cc98efed5340b808ad8f
kaikyalvaro1708 <kaikialvaro57@gmail.com>
2026-05-29 21:39:55 -0300

    initial commit

---
+++ backend/identity-provisioning-api/src/main/java/com/itau/identityprovisioning/IdentityProvisioningApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.identityprovisioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityProvisioningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityProvisioningApiApplication.class, args);
+	}
+
+}
---
+++ backend/identity-provisioning-api/src/test/java/com/itau/identityprovisioning/IdentityProvisioningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.identityprovisioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityProvisioningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
